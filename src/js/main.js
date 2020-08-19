975a591fe7be2ecab5c6b2e6112c8b9650aec752
Paul Coronia <pa.coronia@accelerategroup.net>
2020-08-19 20:43:34 +0800

    Add basic boxes

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,30 @@
+const elements = {
+  background: 'body',
+  character: '#character',
+};
+
 const background = {
   setDaylight: function (daylight) {
-    document.querySelector('body').classList.add(daylight);
+    document.querySelector(elements.background).classList.add(daylight);
   }
 };
 
 const character = {
   setSprite: function (name) {
     const imgUrl = `./assets/sprites/${name}.png`;
-    document.querySelector('img#character').src = imgUrl;
+    document.querySelector(elements.character).src = imgUrl;
   },
 
   setAnim: function (animName) {
-    document.querySelector('img#character').classList.add(animName);
+    document.querySelector(elements.character).classList.add(animName);
   },
 
   tapped: function () {
-    document.querySelector('img#character').classList.remove('anim-floating');
-    document.querySelector('img#character').classList.add('anim-bob');
+    document.querySelector(elements.character).classList.remove('anim-floating');
+    document.querySelector(elements.character).classList.add('anim-bob');
     setTimeout(() => {
-      document.querySelector('img#character').classList.add('anim-floating')
-      document.querySelector('img#character').classList.remove('anim-bob')
+      document.querySelector(elements.character).classList.add('anim-floating')
+      document.querySelector(elements.character).classList.remove('anim-bob')
     }, 500);
   }
 };
@@ -30,5 +35,4 @@ const character = {
   background.setDaylight('day');
   character.setSprite('Indianapolis');
   character.setAnim('anim-floating');
-  preventDrag();
 }) ();
