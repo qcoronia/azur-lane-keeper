235e273d5e1bfd8835f271335b0595c6a1462caa
Paul Coronia <pa.coronia@accelerategroup.net>
2020-09-01 19:45:57 +0800

    Refactor scoping on service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,63 +1,63 @@
-const sw_filesToCache = [
-  '/',
-  '/index.html',
-  
-  '/js/api.js',
-  '/js/config.js',
-  '/js/db.js',
-  '/js/main.js',
-  '/js/service.js',
-  '/js/sw-init.js',
-  
-  '/css/animations.css',
-  '/css/fonts.css',
-  '/css/main.css',
-  
-  '/assets/bg/bg_main_day.png',
-  '/assets/bg/bg_main_night.png',
-  '/assets/bg/bg_main_twilight.png',
-
-  '/assets/fonts/vendor/fontawesome-5.14.0/css/all.min.css',
-  
-  '/assets/fonts/monolisk/Monolisk-Regular.woff2',
-  
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-cyrillic.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-cyrillic-ext.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-greek.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-greek-ext.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-latin.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-latin-ext.woff2',
-  '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-vietnamese.woff2',
-];
-
-const sw_staticCacheName = 'al_keeper_cache';
+const CACHE_NAME = 'al_keeper_cache';
 
 self.addEventListener('install', evt => {
   evt.waitUntil(
-    caches.open(sw_staticCacheName).then(cache => {
-      return cache.addAll(sw_filesToCache);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+
+        '/js/api.js',
+        '/js/config.js',
+        '/js/db.js',
+        '/js/main.js',
+        '/js/service.js',
+        '/js/sw-init.js',
+
+        '/css/animations.css',
+        '/css/fonts.css',
+        '/css/main.css',
+
+        '/assets/bg/bg_main_day.png',
+        '/assets/bg/bg_main_night.png',
+        '/assets/bg/bg_main_twilight.png',
+
+        '/assets/fonts/vendor/fontawesome-5.14.0/css/all.min.css',
+
+        '/assets/fonts/monolisk/Monolisk-Regular.woff2',
+
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-cyrillic.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-cyrillic-ext.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-greek.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-greek-ext.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-latin.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-latin-ext.woff2',
+        '/assets/fonts/source-sans-pro/SourceSansPro-SemiBold-vietnamese.woff2',
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(res => {
-      if (!!res) {
-        return res;
+    caches.match(evt.request).then(cachedResponse => {
+      if (!!cachedResponse) {
+        return cachedResponse;
       }
 
-      return fetch(evt.request).then(res2 => {
-        if (!res2 || res2.status !== 200 || res2.type !== 'basic') {
-          return res2;
+      return fetch(evt.request).then(freshResponse => {
+        if (!freshResponse || freshResponse.status !== 200 || freshResponse.type !== 'basic') {
+          console.warn('failed to cache request: ', evt.request);
+          return freshResponse;
         }
 
-        let responseToBeCached = res2.clone();
-        caches.open(sw_staticCacheName).then(cache => {
-          cache.put(evt.request, responseToBeCached);
+        const freshResponseClone = freshResponse.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(evt.request, freshResponseClone);
           console.log('request cached: ', evt.request);
         });
-        return res2;
+
+        return freshResponse;
       });
     }).catch(err => console.error(err))
   );
