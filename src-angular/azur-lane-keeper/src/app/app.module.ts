ff2a78da172143b732969c2baa336c727cfa2be0
Paul Coronia <pa.coronia@accelerategroup.net>
2020-09-16 16:47:22 +0800

    Implement service worker manually instead of using slightly inconvenient angular-generated one

--- src-angular/azur-lane-keeper/src/app/app.module.ts
+++ src-angular/azur-lane-keeper/src/app/app.module.ts
@@ -7,23 +7,19 @@ import { CoreModule } from './core/core.module';
 import { LoadingScreenComponentModule } from './components/loading-screen/loading-screen-component.module';
 import { HomeBackgroundComponentModule } from './components/home-background/home-background-component.module';
 import { HeaderComponentModule } from './components/header/header-component.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule,
     AppRoutingModule,
 
     CoreModule,
+    LoadingScreenComponentModule,
     HomeBackgroundComponentModule,
     HeaderComponentModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src-angular/azur-lane-keeper/src/main.ts
+++ src-angular/azur-lane-keeper/src/main.ts
@@ -8,7 +8,11 @@ if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-     platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-   });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => console.log('Registration successful, scope is:', registration.scope))
+    .catch(error => console.log('Service worker registration failed, error:', error));
+}
