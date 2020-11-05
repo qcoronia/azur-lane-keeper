524e4f3e1665932641bff3dab4846ba94b94b94d
Paul Coronia <pa.coronia@accelerategroup.net>
2020-11-05 20:42:05 +0800

    Add border on thumbnails

--- src/app/components/shipgirl-thumbnail/shipgirl-thumbnail.component.ts
+++ src/app/components/shipgirl-thumbnail/shipgirl-thumbnail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, AfterContentInit } from '@angular/core';
 import { ShipgirlService } from 'src/app/core/services/shipgirl/shipgirl.service';
-import { map, take } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
+
+const TRANSPARENT_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 
 @Component({
   selector: 'app-shipgirl-thumbnail',
@@ -17,7 +19,7 @@ export class ShipgirlThumbnailComponent implements AfterContentInit {
 
   ngAfterContentInit(): void {
     this.shipgirl.getByName(this.shipName).pipe(
-      map(res => res.thumbnail),
+      map(res => res?.thumbnail || TRANSPARENT_PIXEL),
       take(1),
     ).subscribe(url => this.thumbnailUrl = url);
   }
