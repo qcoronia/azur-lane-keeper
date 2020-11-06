606957dbc948710ff6f7d60ffee53d8f8bf358dc
Paul Coronia <pa.coronia@accelerategroup.net>
2020-11-06 21:38:45 +0800

    Arrange fleet layout platforms

--- src/app/components/fleet-form/fleet-form.component.ts
+++ src/app/components/fleet-form/fleet-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FleetFormation } from 'src/app/core/models/entities/fleet-formation.model';
 
 @Component({
   selector: 'app-fleet-form',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FleetFormComponent implements OnInit {
 
-  constructor() { }
+  public fleetFormation: FleetFormation;
+
+  constructor() {
+    this.fleetFormation = {
+      name: 'sample',
+      main: {
+        flagship: { shipName: 'Long Island', notes: 'flagship'},
+        top: { shipName: '', notes: 'top'},
+        bottom: { shipName: '', notes: 'bottom'},
+        notes: 'main',
+      },
+      vanguard: {
+        lead: { shipName: 'Javelin', notes: 'lead'},
+        middle: { shipName: 'Laffey', notes: 'middle'},
+        last: { shipName: 'Ayanami', notes: 'last'},
+        notes: 'vanguard',
+      },
+      notes: 'sample',
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  public getShipChibiUrl(shipName: string): string {
+    const TRANSPARENT_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+    return TRANSPARENT_PIXEL;
   }
 
 }
